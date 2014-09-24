5cf630eb429eb21ba584fac54851a6f6a65ed5fa
Pavel Kolmykov <pavel.kolmykov@gmail.com>
2014-09-24 19:36:23 +0300

    changing for using URL instead of VERSION in jetty setup

---
+++ setup_jetty.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+URL="http://eclipse.org/downloads/download.php?file=/jetty/stable-9/dist/jetty-distribution-9.2.3.v20140905.tar.gz&r=1"
+
+cd ${OPENSHIFT_DATA_DIR:="~"}
+
+if [ -d "jetty" ] && [ "`cat jetty/URL`" == $URL ]; then
+
+  echo "Jetty already installed"
+
+else
+
+  echo "Installing Jetty from ${URL}"
+
+  if [ -d "jetty" ]; then
+    rm -rf jetty
+  fi
+
+  curl -o jetty.tar.gz ${URL}
+
+  tar -xf jetty.tar.gz -C jetty --strip-components=1
+
+  rm jetty.tar.gz
+
+  echo $URL > jetty/URL
+
+  rm -rf jetty/contexts/*
+  rm -rf jetty/webapps
+
+  curl -o maven.xml -L "https://raw.githubusercontent.com/pkolmykov/jetty-openshift-java8/master/maven.xml"
+
+fi
